946a2acd098aaa35bbff9a3cb55b291a9ddf0c71
姜敏 <jiangmin@cditv.tv>
2023-01-31 10:11:23 +0800

    init

---
+++ src/main/java/pers/helen/spring17/springlearn17/SpringLearn17Application.java
@@ -0,0 +1,13 @@
+package pers.helen.spring17.springlearn17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearn17Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringLearn17Application.class, args);
+    }
+
+}
---
+++ src/test/java/pers/helen/spring17/springlearn17/SpringLearn17ApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.helen.spring17.springlearn17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearn17ApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+
+}
